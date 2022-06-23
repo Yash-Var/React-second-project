07e4ef20b6dc180da6378658ca5af94f2b8ca137
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-06-23 22:17:11 +0530

    start making the raect app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Cards(props) {
+  return (
+    <div className="cards">
+      <div className="card">
+        <img src={props.imgSrc} alt="myPic" className="card__img" />
+        <div className="card__info">
+          <span className="card__category">{props.title}</span>
+          <h3 className="card__title">{props.Name}</h3>
+          <a href={props.href} target="_blank">
+            <button>Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<>
+  <Cards title="A Netflix Original Series" Name="Frame Game" imgSrc="http://surl.li/cguja" href="https://www.netflix.com/in/title/81133092" />
+</>, document.getElementById("root"));
